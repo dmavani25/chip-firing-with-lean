f6d7678277ecb3520254b3bb336e764902750eb7
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2024-10-06 09:41:44 -0400

    Update ChipFiringWithLean.lean

--- ChipFiringWithLean.lean
+++ ChipFiringWithLean.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `ChipFiringWithLean` library.
 -- Import modules here that should be built as part of the library.
 import ChipFiringWithLean.Basic
-import ChipFiringWithLean.CF-Graph
+import ChipFiringWithLean.CFGraph
